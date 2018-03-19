f590976a5563d81beb23e42d1065c5a3e4486bca
ujjwalagr <ujjwalagrawal53@gmail.com>
2018-03-19 19:57:31 +0530

    Splash Added

---
+++ app/src/main/java/rajasthantourism/com/tourrajasthan/Slider.java
@@ -0,0 +1,13 @@
+package rajasthantourism.com.tourrajasthan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Slider extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slider);
+    }
+}
--- app/src/main/java/rajasthantourism/com/tourrajasthan/Splash.java
+++ app/src/main/java/rajasthantourism/com/tourrajasthan/Splash.java
@@ -1,5 +1,7 @@
 package rajasthantourism.com.tourrajasthan;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,14 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(Splash.this,Slider.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        }, 1000);
     }
 }
